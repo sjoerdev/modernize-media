8c4c2a973db49b23bf1e2863645352d46b6b590d
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-09-27 01:17:01 +0200

    Update flatten.ps1

--- flatten.ps1
+++ flatten.ps1
@@ -1,13 +1,30 @@
 $rootDir = "./wdmycloud_backup"
 $flatDir = "./wdmycloud_backup_flat"
 
+# only copy over files of these formats to the flat directory
+$extensions = 
+@(
+    # image formats
+    "bmp", "jpeg", "jpg", "png", "orf", "tif", "tiff",
+
+    # video formats
+    "avi", "mkv", "mod", "mov", "mp4", "mpg", "mpeg", "vob", "wmv"
+)
+
+if (-not (Test-Path $flatDir))
+{
+    New-Item -Path $flatDir -ItemType Directory | Out-Null
+}
+
 Write-Host "Flattening folder: $rootDir"
 
 $files = Get-ChildItem -Path $rootDir -Recurse -File
 
 foreach ($file in $files)
 {
-    $destination = Join-Path -Path $rootDir -ChildPath $file.Name
+    if ($extensions -notcontains $file.Extension.TrimStart(".").ToLower()) { continue }
+
+    $destination = Join-Path -Path $flatDir -ChildPath $file.Name
     $baseName = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
     $extension = $file.Extension
     $counter = 1
@@ -16,12 +33,12 @@ foreach ($file in $files)
     while (Test-Path $destination)
     {
         $newName = "{0}_{1}{2}" -f $baseName, $counter, $extension
-        $destination = Join-Path -Path $rootDir -ChildPath $newName
+        $destination = Join-Path -Path $flatDir -ChildPath $newName
         $counter++
     }
 
-    # move the file
-    Move-Item -Path $file.FullName -Destination $destination
+    # copy the file
+    Copy-Item -Path $file.FullName -Destination $destination
 }
 
 Write-Host "Flattening complete!"
