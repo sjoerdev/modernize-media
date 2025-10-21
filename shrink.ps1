9df98021cd2e70efaefd4fd393fa8b7d8ccd893f
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-10-21 12:19:46 +0200

    blip

---
+++ shrink.ps1
@@ -0,0 +1,51 @@
+# input and output directories
+$InputRoot = "./" + $args[0]
+$OutputRoot = $InputRoot + "_av1"
+
+# video formats
+$VideoFormatsToConvert = @("mp4")
+$VideoFormatToConvertTo = "mkv"
+
+# recursively find all media files
+$AllFiles = @()
+
+foreach ($ext in $VideoFormatsToConvert)
+{
+    $AllFiles += Get-ChildItem -Path $InputRoot -Recurse -Include ("*." + $ext) -File
+}
+
+$TotalFiles = $AllFiles.Count
+$Counter = 0
+
+foreach ($inputFile in $AllFiles)
+{
+    $Counter++
+    $progressPercent = [math]::Round(($Counter / $TotalFiles) * 100, 1)
+
+    # calculate output path
+    $absInputRoot = (Resolve-Path $InputRoot).Path
+    $absFile = (Resolve-Path $inputFile.FullName).Path
+    $relativePath = $absFile.Substring($absInputRoot.Length).TrimStart('\')
+
+    $outputFile = [System.IO.FileInfo]$(Join-Path -Path $OutputRoot -ChildPath ([System.IO.Path]::ChangeExtension($relativePath, ("." + $VideoFormatToConvertTo))))
+
+    # make sure output path exists
+    $outputDir = Split-Path -Path $outputFile.FullName -Parent
+    if (-not (Test-Path $outputDir))
+    {
+        New-Item -Path $outputDir -ItemType Directory -Force | Out-Null
+    }
+
+    # skip already existing files
+    if (Test-Path $outputFile.FullName)
+    {
+        Write-Host "Skipping existing file: $($outputFile.FullName)"
+        continue
+    }
+
+    Write-Host "[$Counter/$TotalFiles] [$progressPercent%] Converting $($inputFile.Name) to $($outputFile.Name)"
+
+    ffmpeg -hide_banner -loglevel error -hwaccel cuda -hwaccel_output_format cuda -i "$($inputFile.FullName)" -c:v av1_nvenc -preset fast -c:a copy "$($outputFile.FullName)"
+}
+
+Write-Host "Finished converting media"
