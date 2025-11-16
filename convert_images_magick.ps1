9f56b96517104baed683e5b8bf0ebd1bf4fc5820
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-11-16 15:22:34 +0100

    Create convert_images_magick.ps1

---
+++ convert_images_magick.ps1
@@ -0,0 +1,51 @@
+# formats to take as imput
+$Includes = @("bmp", "jpeg", "jpg", "png", "orf", "tif", "tiff", "raw", "avif")
+
+# output
+$Format = "jpeg"
+
+# input and output directories
+$InputRoot = "./" + $args[0]
+$OutputRoot = $InputRoot + "_" + "magick" + "_" + $Format
+
+# recursively find all media files
+$AllFiles = @()
+foreach ($ext in $Includes)
+{
+    $AllFiles += Get-ChildItem -Path $InputRoot -Recurse -Include ("*." + $ext) -File
+}
+
+$TotalFiles = $AllFiles.Count
+$Counter = 0
+
+foreach ($inputFile in $AllFiles)
+{
+    $Counter++
+    $progressPercent = [math]::Round(($Counter / $TotalFiles) * 100, 1)
+
+    # calculate output path
+    $absInputRoot = (Resolve-Path $InputRoot).Path
+    $absFile = (Resolve-Path $inputFile.FullName).Path
+    $relativePath = $absFile.Substring($absInputRoot.Length).TrimStart('\')
+    $outputFile = [System.IO.FileInfo]$(Join-Path -Path $OutputRoot -ChildPath ([System.IO.Path]::ChangeExtension($relativePath, ("." + $Format))))
+
+    # make sure output path exists
+    $outputDir = Split-Path -Path $outputFile.FullName -Parent
+    if (-not (Test-Path $outputDir))
+    {
+        New-Item -Path $outputDir -ItemType Directory -Force | Out-Null
+    }
+
+    # skip already existing files
+    if (Test-Path $outputFile.FullName)
+    {
+        Write-Host "Skipping existing file: $($outputFile.FullName)"
+        continue
+    }
+
+    Write-Host "[$Counter/$TotalFiles] [$progressPercent%] [magick] Converting $($inputFile.Name) to $($outputFile.Name)"
+
+    magick "$($inputFile.FullName)" "$($outputFile.FullName)"
+}
+
+Write-Host "Finished converting media"
