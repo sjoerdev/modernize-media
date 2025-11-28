99bbbdc7be3bf31aa1731f69d9d181c0abf0a38b
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-11-28 01:37:37 +0100

    put common logic in a module

--- convert_images_magick.ps1
+++ convert_images_magick.ps1
@@ -1,4 +1,6 @@
-# output
+Import-Module .\convert_media.psm1 -Force
+
+$InputDirectory = $args[0]
 $InputFormats = "formats_images.txt";
 $OutputFormat = "avif"
 $Identifier = "magick"
@@ -7,49 +9,4 @@ $Command = {
     magick $inputFilePath $outputFilePath
 }
 
-# input and output directories
-$InputNameNormalized = $args[0].TrimEnd('\','/').Replace('./','').Replace('.\','')
-$InputRoot = "./" + $InputNameNormalized
-$OutputRoot = $InputRoot + "_" + $OutputFormat + "_" + $Identifier
-
-# formats to take as input
-$FormatsFilePath = Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) $InputFormats;
-$Includes = Get-Content $FormatsFilePath
-
-# recursively find all media files
-$AllFiles = Get-ChildItem -Path $InputRoot -Recurse -File | Where-Object { $Includes -contains $_.Extension.TrimStart(".").ToLower() }
-
-$TotalFiles = $AllFiles.Count
-$Counter = 0
-
-foreach ($inputFile in $AllFiles)
-{
-    $Counter++
-    $progressPercent = [math]::Round(($Counter / $TotalFiles) * 100, 1)
-
-    # calculate output path
-    $absInputRoot = (Resolve-Path $InputRoot).Path
-    $absFile = (Resolve-Path $inputFile.FullName).Path
-    $relativePath = $absFile.Substring($absInputRoot.Length).TrimStart('\')
-    $outputFile = [System.IO.FileInfo]$(Join-Path -Path $OutputRoot -ChildPath ([System.IO.Path]::ChangeExtension($relativePath, ("." + $OutputFormat))))
-
-    # make sure output path exists
-    $outputDir = Split-Path -Path $outputFile.FullName -Parent
-    if (-not (Test-Path $outputDir))
-    {
-        New-Item -Path $outputDir -ItemType Directory -Force | Out-Null
-    }
-
-    # skip already existing files
-    if (Test-Path $outputFile.FullName)
-    {
-        Write-Host "Skipping existing file: $($outputFile.FullName)"
-        continue
-    }
-
-    Write-Host "[$Counter/$TotalFiles] [$progressPercent%] [$Identifier] Converting $($inputFile.Name) to $($outputFile.Name)"
-
-    & $Command "$($inputFile.FullName)" "$($outputFile.FullName)"
-}
-
-Write-Host "Finished converting media"
+Convert-Media $InputFormats $OutputFormat $Identifier $Command $InputDirectory
---
+++ convert_media.psm1
@@ -0,0 +1,51 @@
+# common logic for file conversion
+
+function Convert-Media($InputFormats, $OutputFormat, $Identifier, $Command, $InputDirectory)
+{
+    # input and output directories
+    $InputNameNormalized = $InputDirectory.TrimEnd('\','/').Replace('./','').Replace('.\','')
+    $InputRoot = "./" + $InputNameNormalized
+    $OutputRoot = $InputRoot + "_" + $OutputFormat + "_" + $Identifier
+
+    # formats to take as input
+    $FormatsFilePath = Join-Path $PSScriptRoot $InputFormats
+    $Includes = Get-Content $FormatsFilePath
+
+    # recursively find all media files
+    $AllFiles = Get-ChildItem -Path $InputRoot -Recurse -File | Where-Object { $Includes -contains $_.Extension.TrimStart(".").ToLower() }
+
+    $TotalFiles = $AllFiles.Count
+    $Counter = 0
+
+    foreach ($inputFile in $AllFiles)
+    {
+        $Counter++
+        $progressPercent = [math]::Round(($Counter / $TotalFiles) * 100, 1)
+
+        # calculate output path
+        $absInputRoot = (Resolve-Path $InputRoot).Path
+        $absFile = (Resolve-Path $inputFile.FullName).Path
+        $relativePath = $absFile.Substring($absInputRoot.Length).TrimStart('\')
+        $outputFile = [System.IO.FileInfo]$(Join-Path -Path $OutputRoot -ChildPath ([System.IO.Path]::ChangeExtension($relativePath, ("." + $OutputFormat))))
+
+        # make sure output path exists
+        $outputDir = Split-Path -Path $outputFile.FullName -Parent
+        if (-not (Test-Path $outputDir))
+        {
+            New-Item -Path $outputDir -ItemType Directory -Force | Out-Null
+        }
+
+        # skip already existing files
+        if (Test-Path $outputFile.FullName)
+        {
+            Write-Host "Skipping existing file: $($outputFile.FullName)"
+            continue
+        }
+
+        Write-Host "[$Counter/$TotalFiles] [$progressPercent%] [$Identifier] Converting $($inputFile.Name) to $($outputFile.Name)"
+
+        & $Command "$($inputFile.FullName)" "$($outputFile.FullName)"
+    }
+
+    Write-Host "Finished converting media"
+}
