105490e85a984e693d7028948c561c9e158afce5
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-11-28 23:08:01 +0100

    Update convert_videos_ffmpeg.ps1

--- convert_videos_ffmpeg.ps1
+++ convert_videos_ffmpeg.ps1
@@ -2,11 +2,11 @@ Import-Module .\convert_media.psm1 -Force
 
 $InputDirectory = $args[0]
 $InputFormats = "formats_videos.txt";
-$OutputFormat = "mkv"
+$OutputFormat = "mp4"
 $Identifier = "nvenc"
 $Command = {
     param($inputFilePath, $outputFilePath)
-    ffmpeg -hide_banner -loglevel error -i $inputFilePath -preset fast -c:v hevc_nvenc -cq 20 -c:a aac -b:a 320k $outputFilePath
+    ffmpeg -hide_banner -loglevel error -i $inputFilePath -preset p4 -c:v hevc_nvenc -cq 20 -c:a aac -b:a 320k $outputFilePath
 }
 
 Convert-Media $InputFormats $OutputFormat $Identifier $Command $InputDirectory
