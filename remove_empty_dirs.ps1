201d3aea257f6979386097d603c9496d39b36090
Sjoerd Wouters <59654421+sjoerdev@users.noreply.github.com>
2026-08-17 19:50:11 +0200

    Update remove_empty_dirs.ps1

--- remove_empty_dirs.ps1
+++ remove_empty_dirs.ps1
@@ -1,10 +1,10 @@
 $InputNameNormalized = $args[0].TrimEnd('\','/').Replace('./','').Replace('.\','')
 $rootDir = "./" + $InputNameNormalized
 
-$emptyDirs = ls $rootDir -Directory -Recurse | where { @(ls $_.FullName -Force).Count -eq 0 }
+$emptyDirs = ls $rootDir -Directory -Recurse | where { @(ls $_.FullName -File -Recurse -Force).Count -eq 0 }
 
 foreach ($d in $emptyDirs) {
     if (Test-Path $d.FullName) {
-        Remove-Item $d.FullName -Force
+        Remove-Item $d.FullName -Recurse -Force
     }
 }
